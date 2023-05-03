cfd410f77d80a1c4c3fdab737d0f609cad21a6f2
Ricardo-Zaragoza <119813324+Ricardo-Zaragoza@users.noreply.github.com>
2023-05-02 22:30:57 -0600

    TAREA9

---
+++ Tarea9/Main.java
@@ -0,0 +1,14 @@
+package Tarea9;
+
+import Tarea9.iu.Principal;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Principal ventana = new Principal("Hola mundo AWT");
+
+
+    }
+
+}
