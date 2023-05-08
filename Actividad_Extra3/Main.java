fdeff57f743224eac4d3256d973bff5efa85bea9
Ricardo-Zaragoza <119813324+Ricardo-Zaragoza@users.noreply.github.com>
2023-05-07 18:38:36 -0600

    ACTIVIDAD_EXTRA3

---
+++ Actividad_Extra3/Main.java
@@ -0,0 +1,11 @@
+package Actividad_Extra3;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Ventana frame = new Ventana("Layouts");
+
+        VentanaGrid gridFrame = new VentanaGrid("GridLayout");
+    }
+}
