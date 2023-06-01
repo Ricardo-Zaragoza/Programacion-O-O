63be8cff86be7c3f800a892695ab1a1013f11261
Ricardo-Zaragoza <119813324+Ricardo-Zaragoza@users.noreply.github.com>
2023-06-01 17:05:13 -0600

    Actividad_Extra10

---
+++ Actividad_Extra10/main.java
@@ -0,0 +1,8 @@
+package Actividad_Extra10;
+
+public class main {
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana("Joptionpane");
+        Controlador controlador = new Controlador(ventana);
+    }
+}
